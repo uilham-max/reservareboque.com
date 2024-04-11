dd5a5c36fb7c7095628f00312b1b4f6888b78b03
uilham-max <uilhamgoncalves@gmail.com>
2024-04-11 03:17:30 -0300

    Configurações de segurança do BD com .env v4

--- database/conexao.js
+++ database/conexao.js
@@ -3,14 +3,9 @@ require('dotenv').config()
 const DATABASE = process.env.DATABASE
 const USER = process.env.USER
 const PASSWORD = process.env.PASSWORD
-const URL = process.env.URL
 const DIALECT = process.env.DIALECT
 
-const conexao = new Sequelize(DATABASE, USER, PASSWORD, {
-    host: URL,
-    timezone: '-30:00',
-    logging: false
-})
+const conexao = new Sequelize(`${DIALECT}://${USER}:${PASSWORD}@${HOST}/${DATABASE}`)
 
 
 // const conexao = new Sequelize('reboquesmvc', 'postgres', 'postgres', {

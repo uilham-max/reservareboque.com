2a0428ded2ba4bf678e6f59c540e784bb9edf831
uilham-max <uilhamgoncalves@gmail.com>
2024-04-19 00:12:13 -0300

    Teste de conexão remota usando variáveis de ambiente

--- database/conexao.js
+++ database/conexao.js
@@ -1,31 +1,31 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
-/** Conexão remota com banco de dados do Render.com */
-
-// const conexao = new Sequelize(process.env.RENDER_DB_NOME, process.env.RENDER_DB_USER, process.env.RENDER_DB_PASSWORD, {
-//     host: process.env.RENDER_DB_HOST_EXTERNO,
-//     dialect: process.env.RENDER_DB_DIALECT,
-//     dialectOptions: {
-//         ssl: {
-//             required: true,
-//             rejectUnauthorized: true
-//         }
-//     },
-//     timezone: '-03:00',
-//     logging: false
-// })
-
-/** Esta conexão deve ser usada no GitHub */ 
+/** Conectando (usando) ao BD remoto no Render.com */
 
 const conexao = new Sequelize(process.env.RENDER_DB_NOME, process.env.RENDER_DB_USER, process.env.RENDER_DB_PASSWORD, {
-    host: process.env.RENDER_DB_HOST,
+    host: process.env.RENDER_DB_HOST_EXTERNO,
     dialect: process.env.RENDER_DB_DIALECT,
+    dialectOptions: {
+        ssl: {
+            required: true,
+            rejectUnauthorized: true
+        }
+    },
     timezone: '-03:00',
     logging: false
 })
 
-/**Configuração de conexão com banco de dados local */
+/** Conexão interna do Render.com */ 
+
+// const conexao = new Sequelize(process.env.RENDER_DB_NOME, process.env.RENDER_DB_USER, process.env.RENDER_DB_PASSWORD, {
+//     host: process.env.RENDER_DB_HOST,
+//     dialect: process.env.RENDER_DB_DIALECT,
+//     timezone: '-03:00',
+//     logging: false
+// })
+
+/**Conexão com banco de dados local */
 
 // const conexao = new Sequelize(process.env.DB_NOME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //     host: process.env.DB_HOST,

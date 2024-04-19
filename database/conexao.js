51d0ab8fd188d9fa54a29ebcda14adb9f432c0f0
uilham-max <uilhamgoncalves@gmail.com>
2024-04-19 00:57:32 -0300

    Teste de conexão

--- database/conexao.js
+++ database/conexao.js
@@ -1,22 +1,26 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
-console.log(process.env.RENDER_DB_DIALECT);
+RENDER_DB_NOME = process.env.RENDER_DB_NOME
+RENDER_DB_USER = process.env.RENDER_DB_USER
+RENDER_DB_PASSWORD = process.env.RENDER_DB_PASSWORD
+RENDER_DB_HOST_EXTERNO = process.env.RENDER_DB_HOST_EXTERNO
+RENDER_DB_DIALECT = process.env.RENDER_DB_DIALECT
 
 /** Conectando (usando) ao BD remoto no Render.com */
 
-// const conexao = new Sequelize(process.env.RENDER_DB_NOME, process.env.RENDER_DB_USER, process.env.RENDER_DB_PASSWORD, {
-//     host: process.env.RENDER_DB_HOST_EXTERNO,
-//     dialect: process.env.RENDER_DB_DIALECT,
-//     dialectOptions: {
-//         ssl: {
-//             required: true,
-//             rejectUnauthorized: true
-//         }
-//     },
-//     timezone: '-03:00',
-//     logging: false
-// })
+const conexao = new Sequelize(RENDER_DB_NOME, RENDER_DB_USER, RENDER_DB_PASSWORD, {
+    host: RENDER_DB_HOST_EXTERNO,
+    dialect: RENDER_DB_DIALECT,
+    dialectOptions: {
+        ssl: {
+            required: true,
+            rejectUnauthorized: true
+        }
+    },
+    timezone: '-03:00',
+    logging: false
+})
 
 /** Conexão interna do Render.com */ 
 
@@ -27,7 +31,7 @@ console.log(process.env.RENDER_DB_DIALECT);
 //     logging: false
 // })
 
-const conexao = new Sequelize("postgres://reboquesmvc_user:CScOS7Jmyl7fz2NkQWynsGNFJrUBR0wm@dpg-cobjdm5jm4es739qo6gg-a/reboquesmvc")
+// const conexao = new Sequelize("postgres://reboquesmvc_user:CScOS7Jmyl7fz2NkQWynsGNFJrUBR0wm@dpg-cobjdm5jm4es739qo6gg-a/reboquesmvc")
 
 /**Conexão com banco de dados local */
 

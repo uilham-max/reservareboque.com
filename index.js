404ad7f6b1d9c6cf825b2d5b9290298cbe174228
uilham-max <uilhamgoncalves@gmail.com>
2024-04-28 18:37:36 -0300

    Instalado CORS e Testando requisições da tela de qrcode.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const conexao = require('./database/conexao.js')
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const port = 3000
 
@@ -16,6 +17,7 @@ var { removerPagamentosNaoAprovados } = require('./helpers/removerPagamentosNaoA
 const app = express()
 
 // Para Express 4.16 ou superior
+
 app.use(express.json())
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +27,7 @@ app.use(session({secret: 'udjs93ka0', resave: true, saveUninitialized: true}));
 // Para versões anteriores ao Express 4.16
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use(ClienteController)
 app.use(ReboqueController)

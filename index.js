512105bcaac48503d1f28feca4fd43427acaae3d
uilham-max <uilhamgoncalves@gmail.com>
2024-04-28 21:20:25 -0300

    API trocada de POST para GET. Testando requisições da tela de qrcode.

--- index.js
+++ index.js
@@ -16,28 +16,6 @@ var { removerPagamentosNaoAprovados } = require('./helpers/removerPagamentosNaoA
 
 const app = express()
 
-// Para Express 4.16 ou superior
-
-app.use(express.json())
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
-app.use(session({secret: 'udjs93ka0', resave: true, saveUninitialized: true}));
-
-// Para versões anteriores ao Express 4.16
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-
-
-
-app.use(ClienteController)
-app.use(ReboqueController)
-app.use(ReservaController)
-app.use(AdminController)
-app.use(IndexController)
-app.use(pagamentoController)
-
 // Lista de origens permitidas
 const whitelist = ['https://reboquesoliveira.com', 'https://www.reboquesoliveira.com'];
 
@@ -52,23 +30,43 @@ const corsOptions = {
     }
 };
 
-
-
-
 app.use(cors(corsOptions))
 
-// Como o render não fica mais de 1 minuto no ar, isso remove pagamenos quando ele sobe.
-try{
-    console.log("Removendo pagamentos com data vencida...");
-    removerPagamentosNaoAprovados()
-}catch(erro){
-    console.error(erro);
-}
+// Para Express 4.16 ou superior
+app.use(express.json())
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.use(session({secret: 'udjs93ka0', resave: true, saveUninitialized: true}));
 
-conexao.authenticate().then(()=>{
-    app.listen(port,()=>{
-        console.log(`Servidor rodando http://localhost:${port}`)
+// Para versões anteriores ao Express 4.16
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(ClienteController)
+app.use(ReboqueController)
+app.use(ReservaController)
+app.use(AdminController)
+app.use(IndexController)
+app.use(pagamentoController)
+
+// Como o render não fica mais de 1 minuto no ar, isso remove pagamenos quando ele sobe.
+(async () => {
+    
+    try{
+        console.log("Removendo pagamentos com data vencida...");
+        await removerPagamentosNaoAprovados()
+    }catch(erro){
+        console.error(erro);
+    }
+
+    conexao.authenticate().then(()=>{
+        app.listen(port,()=>{
+            console.log(`Servidor rodando http://localhost:${port}`)
+        })
+    }).catch(() => {
+        console.error("Erro. Banco de dados não iniciado.")
     })
-}).catch(() => {
-    console.error("Erro. Banco de dados não iniciado.")
+
 })
+

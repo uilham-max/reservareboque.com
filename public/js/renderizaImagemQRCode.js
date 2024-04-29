d9046a3502a8d53235bfa412461b4d66358a8dcc
uilham-max <uilhamgoncalves@gmail.com>
2024-04-29 16:35:42 -0300

    Código do renderizaImagemQRCode.js refatorado

--- public/js/renderizaImagemQRCode.js
+++ public/js/renderizaImagemQRCode.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    // Decodifica e exibe a imagem
-    window.onload = function() {
-        const encodedImage = '<%= image %>'; // Obtém a imagem codificada do servidor
-        const imageContainer = document.getElementById('imageContainer');
-        const img = document.createElement('img');
-        img.src = 'data:image/jpeg;base64,' + encodedImage; // Decodifica a imagem e define o src
-        imageContainer.appendChild(img);
-    };
+
+    const inputEncodedImage = document.getElementById('codigoHexaImagem'); // Obtém a imagem codificada do servidor
+    const imgElement = document.getElementById('qrCodeImage');
+    imgElement.src = 'data:image/jpeg;base64,' + inputEncodedImage.value; // Decodifica a imagem e define o src
+
+    // const encodedImage = document.getElementById('codigoHexaImagem'); // Obtém a imagem codificada do servidor
+    // console.log("log:",encodedImage.value)
+    // const imageContainer = document.getElementById('imageContainer');
+    // const img = document.createElement('img');
+    // img.src = 'data:image/jpeg;base64,' + encodedImage.value; // Decodifica a imagem e define o src
+    // imageContainer.appendChild(img);
+    
 })

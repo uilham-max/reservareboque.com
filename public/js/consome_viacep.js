11283bf73324392b7414711096657cd71a8e1deb
PedroGonzalez347 <pedrogonzalezdeoliveira@gmail.com>
2025-03-12 19:02:45 -0300

    Remove um console.log do arquivo consome_viacep.js

--- public/js/consome_viacep.js
+++ public/js/consome_viacep.js
@@ -13,14 +13,12 @@ document.addEventListener('DOMContentLoaded', () => {
         if (cep.length !== 8) {
             return;
         } 
-
         try {
             const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
             if (!response.ok) {
                 throw new Error('Erro ao consultar o CEP.');
             }
             const data = await response.json();
-            console.log(data.erro, "erro")
             if(data.erro == true){
                 throw new Error('CEP inválido.');
             }

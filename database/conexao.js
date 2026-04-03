0cd0bbaaacb86715cb1cb8077b56e34461b26ad7
Uilham de Oliveira <uilhamgoncalves@gmail.com>
2026-04-02 23:58:31 -0300

    Adiciona NODE_ENV às variáveis de ambiente com isProduction na conexão com o banco de dados.

--- database/conexao.js
+++ database/conexao.js
@@ -3,17 +3,24 @@ require('dotenv').config()
 
 console.log("Estabelecendo conexão com o BD...");
 
-const conexao = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST_EXTERNO,
-    dialect: process.env.DB_DIALECT,
-    dialectOptions: {
-        ssl: {
-            required: true,
-            rejectUnauthorized: true
-        }
-    },
-    timezone: '-03:00',
-    logging: false
-})
+const isProduction = process.env.NODE_ENV === 'production';
+
+const conexao = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
+        host: process.env.DB_HOST_EXTERNO,
+        dialect: process.env.DB_DIALECT,
+        dialectOptions: isProduction ? {
+            ssl: {
+                required: true,
+                rejectUnauthorized: true
+            }
+        } : {},
+        timezone: '-03:00',
+        logging: false
+    }
+)
 
 module.exports = conexao
